64e44a9894192dd2a834382e79642162cc899eab
Riotpiaole <Rock.Liang@titus.com>
2020-02-08 13:17:46 -0500

    Add downloading pdf file

--- audio_convert.py
+++ audio_convert.py
@@ -6,15 +6,13 @@ import speech_recognition as sr
 
 def speech_recog(file_name="input_sample/audio/introduction_ml.mp3", duration=10):
     convert_mp3_to_wav("input_sample/audio/introduction_ml.mp3")
-
     result = 0
-
     class_audio = AudioFile("input_sample/audio/introduction_ml.wav")
-    set_trace()
+    print(type(class_audio))
+
     recongizer = Recognizer()
 
     with class_audio as src_audio:
         audio = recongizer.record(src_audio, duration=duration)
         print(recongizer.recognize_google(audio))
 
-speech_recog()
---
+++ input_sample/download.sh
@@ -0,0 +1,1 @@
+curl https://github.com/vkosuri/CourseraMachineLearning/raw/master/home/week-1/lectures/pdf/Lecture1.pdf -o "$(pwd)/pdf_slide/lecture1.pdf"
