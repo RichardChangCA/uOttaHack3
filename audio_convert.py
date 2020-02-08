7766656163b2164ee15f9aad11567c669ef59391
Riotpiaole <Rock.Liang@titus.com>
2020-02-08 10:06:36 -0500

    speech recognition

---
+++ audio_convert.py
@@ -0,0 +1,15 @@
+from utils import convert_mp3_to_wav
+
+from speech_recognition import AudioFile , Recognizer
+import speech_recognition as sr
+
+
+def speech_recog(file_name="input_sample/audio/introduction_ml.mp3", duration=10):
+    convert_mp3_to_wav("input_sample/audio/introduction_ml.mp3")
+
+    class_audio = AudioFile("input_sample/audio/introduction_ml.wav")
+    recongizer = Recognizer()
+
+    with class_audio as src_audio:
+        audio = recongizer.record(src_audio, duration=duration)
+        print(recongizer.recognize_google(audio))
---
+++ utils.py
@@ -0,0 +1,28 @@
+from re import compile , match
+from os.path import exists
+
+from pydub import AudioSegment
+
+r_mp3 = compile(r"(?P<file_name>.+).mp3")
+
+
+def convert_mp3_to_wav(file_name):
+    assert exists(file_name), FileExistsError("Not existing mp3 file %s", file_name)
+    assert match(r_mp3, file_name), ValueError("Not mp3 file %s", file_name)
+
+    mp3_file_name = match(r_mp3, file_name).groupdict()['file_name']
+    if exists( mp3_file_name+ '.wav'):
+        return 0
+
+
+
+    try:
+        sound = AudioSegment.from_mp3(file_name)
+        sound.export(mp3_file_name + '.wav', format="wav")
+    except:
+        print("Fail to conver to wave file")
+        return -1
+    return 0
+
+
+
